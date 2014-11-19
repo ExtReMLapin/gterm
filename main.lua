aff02290c49eb7506b3dbca50fd3e8ad8489ab31
Lapin <ExtReMLapin@users.noreply.github.com>
2014-11-19 17:15:59 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -99,6 +99,7 @@ function GTerm.cd(ply, cmd, args) -- darn
 			if (file.IsDir(GTerm.Path ..string.Right(args[1], string.len(args[1]) - 3) .. "/", "BASE_PATH")) then
 				GTerm.Path = GTerm.Path .. string.Right(args[1], string.len(args[1]) - 3)
 			end
+			GTerm.Path = GTerm.Path .. "/"
 		end
 	end
 end
